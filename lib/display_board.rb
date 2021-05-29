ab0dc01328af822d9dae3c485d92be10eefad55e
Alishan  Prasala <alishanp86@gmail.com>
2021-05-29 23:46:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,1 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board
-puts "A Tic Tac Toe Board"
-end
-   end
-=> :display_board
-004:0 > display_board
-A Tic Tac Toe Board
-=> nil
